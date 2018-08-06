7c255bb38b40ed68ea23adb6a88f5b20ffa6bc87
Rollin Shultz <rollinshultz@outlook.com>
2018-08-06 09:55:05 -0400

    Add files via upload

---
+++ learningArrays/learningArrays/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            int sum;
+            numbers = Init_Array(numbers);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            sum = GetSum(numbers);
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+
+        public static int GetSum(int[] numbers)
+        {
+            int sum = numbers.Sum();
+            return sum;
+        }
+
+        public static int[] Init_Array(int[] numbers)
+        {
+            numbers[0] = 2;
+            int sum = 2;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = sum;
+                sum *= 2;
+               
+            }
+            return numbers;
+        }
+
+    };
+}
